b1565c6b370be66461960123dd23a8518429fabe
Priyansh Agarwal <priyanshAgarwal@users.noreply.github.com>
2021-12-24 03:26:21 -0800

    14. Longest Common Prefix

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -8,7 +8,6 @@ Write a function to find the longest common prefix string amongst an array of st
 
 If there is no common prefix, return an empty string "".
 
- 
 
 Example 1:
 
@@ -19,7 +18,6 @@ Example 2:
 Input: strs = ["dog","racecar","car"]
 Output: ""
 Explanation: There is no common prefix among the input strings.
-
 """
 
 #Method 1

1e58466ea3792532ca7c257fd6dbf555da7b9d70
Priyansh Agarwal <priyansh1990@gmail.com>
2021-12-29 16:21:38 -0800

    Pull Request

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -4,12 +4,6 @@
 217. Contains Duplicate
 Easy
 
-3157
-
-904
-
-Add to List
-
 Share
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 

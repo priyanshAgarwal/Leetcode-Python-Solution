4e556e723703019e16534c1ac7e0c7166ae070f9
Priyansh Agarwal <priyansh1990@gmail.com>
2022-04-02 23:08:21 -0700

    Prime or Not

--- Prime or Not.py
+++ Prime or Not.py
@@ -1,15 +1,15 @@
 # cook your dish here
 
 """
-
+Prime or Not
 """
 
 # Python program to check if the input number is odd or even.
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
 
-num = int(input("Enter a number: "))
+num = int(56)
 if (num % 2) == 0:
-   print("{0} is Even".format(num))
+   print(f"{num} is Even")
 else:
-   print("{0} is Odd".format(num))
+   print(f"{num} is Odd")

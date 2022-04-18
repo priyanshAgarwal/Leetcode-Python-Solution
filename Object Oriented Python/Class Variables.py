99df35faa97fe1082408e59beadfb1d870fa1848
Priyansh Agarwal <priyansh1990@gmail.com>
2022-04-18 02:20:16 -0700

    Inheritance

--- Object Oriented Python/Class Variables.py
+++ Object Oriented Python/Class Variables.py
@@ -8,12 +8,14 @@ https://www.youtube.com/watch?v=BJ-VvGyQxho
 class Employee:
 
     raise_amount=1.04
+    static number_of_employee=0
 
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.email = first + '.' + last + '@email.com'
         self.pay = pay
+        Employee.number_of_employee+=1
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
@@ -34,3 +36,13 @@ print(f'Salary is $ {emp_2.pay}')
 emp_2.raise_amount=1.05
 print(f'Amount Raise % {emp_2.raise_amount}')
 print(f'New salary after Raise is $ {emp_2.apply_raise()}')
+
+
+print(f'Number of Employee are {emp_2.number_of_employee}')
+emp_2.number_of_employee=5 
+print(f'Number of Employee are {emp_2.number_of_employee}')
+
+print(f'Number of Employee are {Employee.number_of_employee}')
+Employee.number_of_employee=5
+print(f'Number of Employee are {Employee.number_of_employee}')
+

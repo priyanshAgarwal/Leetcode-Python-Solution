9c33cb306b25a66ff38c2ca7603e58fd0a148a93
Priyansh Agarwal <priyansh1990@gmail.com>
2022-05-20 23:09:08 -0700

    9. Palindrome Number

--- Test  copy 12.py
+++ Test  copy 12.py
@@ -3,3 +3,14 @@
 """
 
 """
+
+num = -5
+
+if num > 1:
+    for i in range(2, num, 1):
+        if num % i == 0:
+            print(f"{num} is not prime, {num} is divisible by {i} and output is {num/i}")
+            break
+    else:
+        print(f"{num} is a prime number")
+else: print(f"{num} is not a prime number")

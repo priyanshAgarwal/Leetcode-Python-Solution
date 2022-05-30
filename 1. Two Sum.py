8e2dcd1146267a152495d82dfbde5904b7358a15
Priyansh Agarwal <priyansh1990@gmail.com>
2022-05-29 21:06:58 -0700

    819. Most Common Word

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -58,7 +58,6 @@ class Solution:
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap={}
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if target-nums[i]==nums[j]:
---
+++ 977. Squares of a Sorted Array.py
@@ -0,0 +1,28 @@
+# cook your dish here
+
+"""
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+"""
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        sq_nums=[x*x for x in nums]
+        return (sorted(sq_nums))
+
+
+# b) Making a new array, not in place, O(n) auxillary space.
+def sortedSquares(self, A: List[int]) -> List[int]:
+        return sorted([v**2 for v in A])

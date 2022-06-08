67770fc57dd226cd802eeaafb52f0ce2269332cf
Priyansh Agarwal <priyansh1990@gmail.com>
2022-06-08 00:30:35 -0700

    Gender With Generous Reviews

---
+++ Pyhton Pandas/Test copy 14.py
@@ -0,0 +1,18 @@
+# cook your dish here
+
+"""
+Gender With Generous Reviews
+
+
+Interview Question Date: June 2020
+
+Airbnb
+Easy
+Interview Questions
+ID 10149
+6
+0
+Write a query to find which gender gives a higher average review score when writing reviews as guests. Use the from_type column to identify guest reviews. Output the gender and their average review score.
+
+
+"""

7fd03991a3bd714aa0bc8b2b5b4e44b27b14ad84
Priyansh Agarwal <priyansh1990@gmail.com>
2022-06-10 01:39:26 -0700

    Number Of Unique Facilities And Inspections Per Municipality

---
+++ Pyhton Pandas/Number Of Unique Facilities And Inspections Per Municipality.py
@@ -0,0 +1,32 @@
+# cook your dish here
+
+"""
+Number Of Unique Facilities And Inspections Per Municipality
+
+
+Interview Question Date: April 2018
+
+City of Los Angeles
+Medium
+Interview Questions
+ID 9702
+3
+1
+Count the number of unique facilities per municipality zip code along with the number of inspections. Output the result along with the number of inspections per each municipality zip code. Sort the result based on the number of inspections in descending order.
+
+
+"""
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+
+df=los_angeles_restaurant_health_inspections
+# Only get first 5 letters
+df['facility_zip']=df['facility_zip'].str[:5]
+
+# groupby one column and then compute on other columns as well 
+df=los_angeles_restaurant_health_inspections.groupby(['facility_zip']).agg({
+    'facility_id':'nunique','record_id':'count'}).reset_index()
+df.sort_values('record_id',ascending=False)

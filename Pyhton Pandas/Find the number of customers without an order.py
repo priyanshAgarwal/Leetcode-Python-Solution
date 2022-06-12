ade88fd269d2503becc654a5466a03a38e4c1d84
Priyansh Agarwal <priyanshAgarwal@users.noreply.github.com>
2022-06-12 01:35:24 -0700

    Find the percentage of shipable orders

--- Pyhton Pandas/Find the number of customers without an order.py
+++ Pyhton Pandas/Find the number of customers without an order.py
@@ -19,6 +19,8 @@ import pandas as pd
 # Start writing code
 df=customers.merge(orders, how='left', left_on='id',right_on='cust_id')
 
-df=df.loc[df['id_y'].isnull()]
+#Only selecting rows where id_y is null
+df=df.loc[df['id_y'].isnull()] 
+
 
 len(df['id_x'].drop_duplicates())
---
+++ Pyhton Pandas/Find the percentage of shipable orders.py
@@ -0,0 +1,28 @@
+# cook your dish here
+
+"""
+Find the percentage of shipable orders
+
+
+Amazon
+Medium
+General Practice
+ID 10090
+5
+0
+Find the percentage of shipable orders.
+Consider an order is shipable if the customer's address is known.
+
+
+"""
+
+import pandas as pd
+
+# Start writing code
+df = pd.merge(orders, customers, how='left', left_on='cust_id', right_on='id')
+df['is_ship']=(df['address'].notnull())
+df['shipable'] = df['address'].isna().astype(int)
+
+df['is_shipable']=df['address'].notnull().astype(int)
+
+100.0*df['is_shipable'].sum()/len(df['is_shipable'])

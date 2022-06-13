643a3830df9496676d4176293d86762324453ef0
Priyansh Agarwal <priyansh1990@gmail.com>
2022-06-13 01:38:42 -0700

    Second Highest Salary

--- Pyhton Pandas/Favorite Customer.py
+++ Pyhton Pandas/Favorite Customer.py
@@ -3,21 +3,18 @@
 """
 Favorite Customer
 
-
 Interview Question Date: May 2019
 
 Amazon
+
 Medium
+
 Interview Questions
-ID 9910
-2
-0
+
 Find "favorite" customers based on the order count and the total cost of orders.
 A customer is considered as a favorite if he or she has placed more than 3 orders and with the total cost of orders more than $100.
 
 Output the customer's first name, city, number of orders, and total cost of orders.
-
-
 """
 
 # Import your libraries

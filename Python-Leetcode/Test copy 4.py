b17b866a10d0efd448006aba873832c1020778b8
Priyansh Agarwal <priyanshAgarwal@users.noreply.github.com>
2024-02-16 03:19:07 -0800

    Just testing Stuff

--- Python-Leetcode/Test copy 4.py
+++ Python-Leetcode/Test copy 4.py
@@ -1,22 +1,14 @@
-# cook your dish here
-
-"""
-
-"""
-s ="]"
-
-array_para=[]
-
-dict_para ={
-    '(':')',
-    '{':'}',
-    '[':']'
-}
 
 
-for bracket in s:
-    if bracket in dict_para.keys():
-        array_para.append(bracket)
-    elif dict_para.get(array_para.pop())==bracket:
-        array_para.pop()
+# aa=[1,1,2]
 
+# print(f"Starting length of array is {len(aa)}")
+# i=0
+# while i<len(aa):
+#     print(f"Length of array is {aa[i]}, {aa[i-1]} popped elment is {aa.pop(-1)} value of i is {i}")
+#     i+=1
+
+
+n,v=divmod(-15,10)
+
+print(n,v)

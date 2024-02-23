8935b61d773640459c63add7ab3d7a9bc37b6ba7
Priyansh Agarwal <priyanshAgarwal@users.noreply.github.com>
2024-02-22 17:05:46 -0800

    219. Contains Duplicate II

--- Python-Leetcode/Test copy 7.py
+++ Python-Leetcode/Test copy 7.py
@@ -1,14 +1,48 @@
-import random
-import string
+'''
+219. Contains Duplicate II
+Easy
+5.9K
+3K
+company
+Amazon
+company
+Facebook
+company
+Adobe
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
-length=12
+ 
 
-letters = string.ascii_lowercase 
-result_str = ''.join(random.choice(letters) for i in range(length))
+Example 1:
 
-aa=''
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
 
-for i in range(length):
-    aa+=random.choice(letters)
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
 
-print(aa)
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+0 <= k <= 105
+
+'''
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+
+        num_dict:dict={}
+
+        for i in range(0,len(nums)):
+            if nums[i] in num_dict and abs(num_dict[nums[i]]-i)<=k:
+                return True
+            num_dict[nums[i]]=i
+        
+        return False
